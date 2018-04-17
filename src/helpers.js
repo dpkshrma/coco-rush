a8763ef300ab18669951ce694c94a40b899f192f
Deepak Sharma <dpkshrma@users.noreply.github.com>
2018-04-17 18:25:40 +0530

    shuffle entire chocolates array

--- src/helpers.js
+++ src/helpers.js
@@ -6,7 +6,7 @@ function getChocolates() {
     const chocosValues = Array.from({ length: 10 }, (v, i) => i + 1);
     return chocosValues.map(value => ({ value, id: shortid() }));
   }
-  return shuffle(getChocoMap()).concat(getChocoMap());
+  return shuffle(getChocoMap().concat(getChocoMap()));
 }
 
 export default {
