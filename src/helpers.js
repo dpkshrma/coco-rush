7a437174c64786479484d592a717b83b11ef176c
Deepak Sharma <dpkshrma@users.noreply.github.com>
2018-04-18 23:40:25 +0530

    update choco img paths

--- src/helpers.js
+++ src/helpers.js
@@ -12,7 +12,7 @@ import shortid from "shortid";
  */
 function getChocolates() {
   function getChocoMap() {
-    const chocosValues = Array.from({ length: 10 }, (v, i) => i + 1);
+    const chocosValues = Array.from({ length: 10 }, (v, i) => i);
     return chocosValues.map(value => ({ value, id: shortid() }));
   }
   return shuffle(getChocoMap().concat(getChocoMap()));
--- src/images/index.js
+++ src/images/index.js
@@ -1,18 +1,18 @@
 import hersheys from "./hersheys.jpg";
-import kitkat from "./kitkat.jpeg";
-import dairyMilk from "./dairyMilk.jpg";
+import kitkat from "./kitkat.jpg";
+import dairymilk from "./dairymilk.jpg";
 import fivestar from "./fivestar.jpg";
-import snickers from "./snickers.jpeg";
+import snickers from "./snickers.jpg";
 import galaxy from "./galaxy.jpg";
-import mars from "./mars.jpeg";
-import lindt from "./lindt.jpeg";
-import bournville from "./bournville.jpeg";
-import barone from "./barone.jpeg";
+import mars from "./mars.jpg";
+import lindt from "./lindt.jpg";
+import bournville from "./bournville.jpg";
+import barone from "./barone.jpg";
 
 export const chocoImages = [
+  dairymilk,
   hersheys,
   kitkat,
-  dairyMilk,
   fivestar,
   snickers,
   galaxy,
